46279c355ccedf33ffe0e81a07db5b455793fd87
SezioS <118485549+sergiovks@users.noreply.github.com>
2023-09-05 11:47:26 +0200

    Update bugbountyenum.sh

--- bugbountyenum.sh
+++ bugbountyenum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Bash script for bug bounty recon using subfinder, httpx, dirsearch, gau, waybackurls, nmap, findomain, vulscan & nmap_vulners
+# Bash script for bug bounty recon using various tools
 
 function ctrl_c(){
   echo -e "\n\n [!] Leaving...\n"
@@ -10,66 +10,67 @@ function ctrl_c(){
 # Ctrl+C
 trap ctrl_c INT
 
-#Get the target domain
-echo "Please enter the target domain:"
-read domain
-echo "A directory named bugbountyEnum will be created right here..."
-mkdir bugbountyEnum
-cd bugbountyEnum
+# Validation
+if [ -z "$1" ]; then
+  echo "Usage: $0 <target_domain>"
+  exit 1
+fi
 
-#Run subfinder to find subdomains
+target_domain="$1"
 
+# Create a directory for bug bounty enumeration
+enum_dir="bugbountyEnum_$(date +'%Y%m%d_%H%M%S')"
+mkdir "$enum_dir"
+cd "$enum_dir"
+
+# Run subfinder to find subdomains
 echo "Running subfinder..."
-subfinder -d $domain -o subdomains.txt
+subfinder -d "$target_domain" -o subdomains.txt
 
-#Run httpx on all discovered domains
+# Run additional subdomain enumeration tools (improvement 1)
+echo "Running additional subdomain enumeration tools..."
+assetfinder --subs-only "$target_domain" | sort -u >> subdomains.txt
+amass enum -d "$target_domain" | sort -u >> subdomains.txt
+crtsh "$target_domain" | sort -u >> subdomains.txt
 
-echo "Running httpx, please be patient you only have to sit down and take a coffee..."
-for i in $(cat subdomains.txt); do
-	httpx -silent -title -threads 10 -follow-redirects -status-code -vhost -retries 3 -timeout 8 -recursive -o "$i_httpx" $i;
+# Run findomain
+echo "Running findomain..."
+findomain -t "$target_domain" -q | sort -u >> subdomains.txt
+
+# Run httpx on discovered domains
+echo "Running httpx..."
+cat subdomains.txt | httpx -silent -title -threads 10 -follow-redirects -status-code -vhost -retries 3 -timeout 8 -recursive -o httpx.txt
+
+# Run dirsearch on discovered subdomains
+echo "Running dirsearch..."
+python3 /usr/lib/python3/dist-packages/dirsearch/dirsearch.py -L subdomains.txt -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt -e html,js,txt,php -t 100 -x 400,403,404,405 -f --plain-text-report dirsearch.txt
+
+# Run nmap scans for TCP and UDP ports (improvement 2)
+echo "Running nmap TCP scans..."
+nmap -p- --min-rate 1000 "$target_domain" -oN nmap_tcp.txt
+echo "Running nmap UDP scans..."
+nmap -sU --top-ports 100 "$target_domain" -oN nmap_udp.txt
+
+# Run gau on all subdomains (improvement)
+echo "Running gau on all subdomains..."
+for subdomain in $(cat subdomains.txt); do
+  gau "$subdomain" --fc 400,403,404,405 --from 202101 --mt html,js,txt,php --o "$subdomain_gau.txt" --providers wayback,commoncrawl,otx,urlscan --retries 3 --timeout 8 --subs --threads 100 --to 202101
 done
 
-#Run dirsearch on all discovered subdomains
-
-echo "Running dirsearch to enum all the subdomains discovered... relax..."
-for i in $(cat subdomains.txt); do
-	python3 /usr/lib/python3/dist-packages/dirsearch/dirsearch.py -u $i -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt -e html,js,txt,php -t 100 -x 400,403,404,405 -f --plain-text-report=$i_dirsearch.txt;
+# Run Nikto on all subdomains
+echo "Running Nikto on all subdomains..."
+for subdomain in $(cat subdomains.txt | sort -u); do
+  echo "Running Nikto on $subdomain..."
+  nikto -h "$subdomain" -output "nikto_$subdomain.txt"
 done
 
-#Run gau 
-
-echo "Running gau, please be patient..."
-gau $domain --fc 400,403,404,405 --from 202101 --mt html,js,txt,php --o gau.txt --providers wayback,commoncrawl,otx,urlscan --retries 3 --timeout 8 --subs --threads 100 --to 202101
-
-#Run findomain
-
-echo "Running findomain to enumerate more subdomains..."
-findomain -t $domain
-
-#Run nmap
-
-echo "Running nmap, this may take a while..."
-echo "Enumerating directories on the domain given at the start..."
-nmap --script http-enum $domain -oN httpenumDomain
-
-echo "Enumerating directories on the subdomains acquired from findomain..."
-for dominio in $(findomain -t $domain); do
-	nmap --script http-enum $dominio done -oN httpenumSubdomains
+# Run nmap scans for discovered subdomains (improvement 6)
+echo "Running nmap scans for discovered subdomains..."
+for subdomain in $(cat subdomains.txt | sort -u); do
+  echo "Running nmap scans for $subdomain..."
+  nmap --script http-enum "$subdomain" -oN "httpenum_$subdomain.txt"
+  nmap --script http-vulners-regex.nse --script-args paths={"/"} "$subdomain" -oN "vulners_$subdomain.txt"
+  nmap -sV --script=vulscan/vulscan.nse "$subdomain" --script-args vulscanoutput='ID: {id} - Title: {title} - Version: {version} - Link: {link} ({matches})\n' -oN "vulscan_$subdomain.txt"
 done
 
-echo "Enumerating possible vulnerabilities on the domain given..."
-nmap --script http-vulners-regex.nse [--script-args paths={"/"}] $domain -oN vulnersDomain
-
-echo "Enumerating possible vulnerabilities on the subdomains from findomain..."
-for dominio in $(findomain -t $domain); do
-	nmap --script http-vulners-regex.nse [--script-args paths={"/"}] $dominio -oN vulnersSubdomain
-done
-
-echo "Enumerating services vulnerabilities for the domain given at the start..."
-nmap -sV --script=vulscan/vulscan.nse $domain -oN vulscandomain
-
-echo "Enumerating services vulnerabilities for the subdomains acquired from findomain ..."
-for dominio in $(findomain -t $domain); do
-
-	nmap -sV --script=vulscan/vulscan.nse $dominio --script-args vulscanoutput='ID: {id} - Title: {title} - Version: {version} - Link: {link} ({matches})\n' -oN vulscansubdomains
-done
+echo "Bug bounty enumeration and scanning completed."
